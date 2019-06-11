08fc0ad4eed0eb007813ed5c9b037c3550efb43a
HeHaowei <35795685+HeHaowei@users.noreply.github.com>
2019-06-11 17:02:13 +0800

    Create Letter Combinations of a Phone Number.cpp

---
+++ main/Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        
+        //dfs
+        vector<string> fi_result;
+        if (digits.size() == 0) return fi_result;
+        string current = "";
+      return dfs(digits,current);
+        
+        
+    
+    }
+    
+    vector <string> result;
+    
+    vector<string> dfs(string digits, string current)
+    {
+       //static vector <string> result; 
+       if (digits.size() == 0) 
+       {
+           result.push_back(current);
+           return result;
+           
+       }
+        else
+        {
+            int num = 3;
+            char ch;
+            if (digits[0] == '7')
+            {
+                num = 4;
+                ch = 'p';
+            }
+            else if (digits[0] == '8')
+            {
+                num = 3;
+                ch = 't';
+            }
+            else if (digits[0] == '9')
+            {
+                num = 4;
+                ch = 'w';
+            }
+            else
+            {
+                num = 3;
+                ch = 3*digits[0] - 53;
+            }
+            
+            for (;num>0;ch++,num--)
+            {
+                string new_current = current;
+                new_current.append(1,ch);
+                result = dfs(&digits[1],new_current);
+            }
+            
+            return result;
+        }
+    }
+    
+};
