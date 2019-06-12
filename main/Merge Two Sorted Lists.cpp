08874d77d0959f018c2e1f29f7fc95165f73830d
HeHaowei <35795685+HeHaowei@users.noreply.github.com>
2019-06-12 21:11:26 +0800

    Create Merge Two Sorted Lists.cpp

---
+++ main/Merge Two Sorted Lists.cpp
@@ -0,0 +1,91 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *head = NULL;
+        ListNode *pre = NULL;
+        
+        while (l1 != NULL || l2 != NULL)
+        {
+            if (l1 != NULL && l2 != NULL)
+            {
+                if (l1->val < l2->val)
+                {
+                  if (head == NULL) 
+                    {
+                        head = l1;
+                        pre = l1;
+                        l1 = l1 -> next;
+                    }
+                    else
+                    {
+                        pre -> next = l1;  
+                        l1 = l1 -> next;
+                        pre = pre -> next;
+                    }
+                }
+                
+                else
+                {
+                    if (head == NULL) 
+                    {
+                        head = l2;
+                        pre = l2;
+                       l2 = l2 -> next;
+                    }
+                    else
+                    {
+                        pre -> next = l2;  
+                        l2 = l2 -> next;
+                        pre = pre -> next;
+                    } 
+                }
+            }
+            
+            
+                else if (l2 == NULL)
+                {
+                    if (head == NULL) 
+                    {
+                        head = l1;
+                        pre = l1;
+                        l1 = l1 -> next;
+                    }
+                    else
+                    {
+                        pre -> next = l1;  
+                        l1 = l1 -> next;
+                        pre = pre -> next;
+                    }
+    
+                }
+                
+                else
+                {
+                   if (head == NULL) 
+                    {
+                        head = l2;
+                        pre = l2;
+                       l2 = l2 -> next;
+                    }
+                    else
+                    {
+                        pre -> next = l2;  
+                        l2 = l2 -> next;
+                        pre = pre -> next;
+                    } 
+                }
+            
+        }
+        
+        return head;
+        
+    }
+};
